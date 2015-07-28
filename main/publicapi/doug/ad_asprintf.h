91243695d609b4678a41db5449ca0155e7e63055
Doug Albright <doug.albright@adtran.com>
2015-07-28 10:06:36 -0500

    Add initial files

---
+++ main/publicapi/doug/ad_asprintf.h
@@ -0,0 +1,14 @@
+/*
+ * ad_asprintf.h
+ *
+ *  Created on: Jun 17, 2015
+ *      Author: root
+ */
+
+#ifndef AD_ASPRINTF_H_
+#define AD_ASPRINTF_H_
+
+
+int ad_asprintf(char **ret, const char *fmt, ...);
+
+#endif /* AD_ASPRINTF_H_ */
